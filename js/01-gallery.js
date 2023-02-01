71b2cd1e22ee2a67826fbb89becdc3047fa4d9ec
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-02-01 23:06:57 +0200

    work7-close escape
    add  close onclickkey Eskape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-// console.log(galleryItems);
 const cardMarkup = makeGalleryMarkup(galleryItems);
 const galleryContainer = document.querySelector(".gallery");
 galleryContainer.insertAdjacentHTML("beforeend", cardMarkup);
@@ -11,7 +11,7 @@ function makeGalleryMarkup(cards) {
     .map(({ preview, original, description }) => {
       return `
     <div class="gallery__item">
-    <a class="gallery__link" href="${original}">
+    <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
         <img
         class="gallery__image"
         src="${preview}"
@@ -27,16 +27,32 @@ function makeGalleryMarkup(cards) {
 
 //Делегування
 
-galleryContainer.addEventListener("click", onGalleryContainer);
+galleryContainer.addEventListener("click", onChangeImageSrc);
 
-function onGalleryContainer(evt) {
-  const isImageGalleryEl = evt.target.classList.contains(gallery__image);
-  if (!isImageGalleryEL) {
+function onChangeImageSrc(evt) {
+  //переформатовуємо закінчення файла  _340.jpg у 1280.jpg
+  let srcImg = evt.target.src;
+  srcImg = srcImg.slice(0, srcImg.length - 8) + "1280.jpg";
+
+  openModal(srcImg);
+}
+
+function openModal(link) {
+  const instance = basicLightbox.create(`
+      <div class="modal">
+        <img width="1400" height="900" src="${link}">
+      </div>
+    `);
+  instance.show();
+  window.addEventListener("keydown", onclickEscape);
+}
+
+function onclickEscape(evt) {
+  if (evt.key !== "Escape") {
     return;
   }
 
-  openModal();
-  console.log(evt.target);
+  window.removeEventListener("keydown", onclickEscape);
+  const closeModal = document.querySelector(".basicLightbox--visible");
+  closeModal.remove("basicLightbox--visible");
 }
-
-// rel = ”noopener noreferrer”
