a9e4fdf88fa90a0395c455093f881b000eaebf8b
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-02-02 00:17:46 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,46 +10,13 @@ function makeGalleryMarkup(cards) {
     .map(({ preview, original, description }) => {
       return `
     <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
-    <img class="gallery__image" src="${preview}" alt="${description}" />
+    <img class="gallery__image" src="${preview}" alt="" title="${description}" />
     </a>
      `;
     })
     .join("");
 }
 
-var lightbox = new SimpleLightbox(".gallery a", {
-  overlay: true,
-  overlayOpacity: 0.7,
-  enableKeyboard: true,
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
 });
-//Делегування
-
-galleryContainer.addEventListener("click", onChangeImageSrc);
-
-function onChangeImageSrc(evt) {
-  //переформатовуємо закінчення файла  _340.jpg у 1280.jpg
-  let srcImg = evt.target.src;
-  srcImg = srcImg.slice(0, srcImg.length - 8) + "1280.jpg";
-
-  openModal(srcImg);
-}
-
-function openModal(link) {
-  const instance = basicLightbox.create(`
-      <div class="modal">
-        <img width="1400" height="900" src="${link}">
-      </div>
-    `);
-  instance.show();
-  window.addEventListener("keydown", onclickEscape);
-}
-
-function onclickEscape(evt) {
-  if (evt.key !== "Escape") {
-    return;
-  }
-
-  window.removeEventListener("keydown", onclickEscape);
-  const closeModal = document.querySelector(".basicLightbox--visible");
-  closeModal.remove("basicLightbox--visible");
-}
